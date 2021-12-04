c234fc68c757a39e0fa48fa4484fa519857f02fc
Łukasz Stępień <stepien86@gmail.com>
2021-12-04 13:12:20 +0100

    crate view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1> index </h1>
+
+    <table>
+        <th>sss</th>
+        <th>sss</th>
+        <th>sss</th>
+    <tbody>
+        <tr>
+            <td>ddd</td>
+        </tr>
+    </tbody>
+    </table>
+</body>
+</html>
